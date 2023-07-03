fc29a03ff23710af23659c862ad43466f949141c
bassantsaied24 <137728394+bassantsaied24@users.noreply.github.com>
2023-07-04 01:59:46 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,40 +1,27 @@
 
 import streamlit as st
-
-
+from main_def import main_def
 
 st.image("iti.jpg")
+st.header("Get your Attendance")
 
-st.header("Get your Attandance")
+page = 0
 
-# st.image("784329.png")
-# #st.audio("Audio.mp3")
-# st.video("Snapchat-1670615673.mp4")
-# st.checkbox('yes')
-# st.button('Click')
-# st.radio('Pick your gender',['Male','Female'])
-# st.selectbox('Pick your gender',['Male','Female'])
-# st.multiselect('choose a planet',['Jupiter', 'Mars', 'neptune'])
-# st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
-# # st.slider('Pick a number', 0,50)
-# st.number_input('Pick a number', 0,10)
-# st.text_input('Email address')
-# st.date_input('Travelling date')
-# st.time_input('School time')
-# st.text_area('Description')
-# st.file_uploader('Upload a photo')
-# st.color_picker('Choose your favorite color')
-st.balloons()
-st.progress(10)
-# with st.spinner('Wait for it...'):
-#     time.sleep(10)
-import os
-import pickle
-import numpy as np
+def main():
 
-if st.button("Get your Attandance"):
-  import imgBackground
+    global page
 
+    if st.button("Get your Attendance"):
+        page = 1
+    if st.button("Stop"):
+        page = 2
+    if page == 1:
+        main_def()
+    elif page == 2:
+        st.title("Already Remarked!!!!!!")
+
+if __name__ == "__main__":
+    main()
 
 
 
